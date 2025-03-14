e60d7cf461214b12c615f2b9434181a475308e5b
Shovon-07 <aljubair.shovon.07@gmail.com>
2025-03-14 10:53:22 +0600

    Committed some changes

--- Unlimited_Message_send.py
+++ Unlimited_Message_send.py
@@ -1,11 +1,12 @@
 import pyautogui
 import time
 
-time.sleep(1)
+time.sleep(5)
 count = 1
 
-while count<=10 :
+while count<=10:
   pyautogui.typewrite("Auto send message " + str(count))
-  time.sleep(2)
+  time.sleep(1)
+  pyautogui.press("esc")
   pyautogui.press("enter")
   count+=1
